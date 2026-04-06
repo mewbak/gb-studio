59f75c3647a1b273eb0e5dbeb19546c528a57dc8
Chris Maltby <chris.maltby@gmail.com>
2026-04-06 18:01:34 +0100

    Remove languages without enough localisation of music editor

--- src/apps/gbs-music-web/lib/preferences.ts
+++ src/apps/gbs-music-web/lib/preferences.ts
@@ -14,48 +14,35 @@ export const webThemeOptions = [
 
 const webLocaleLookup = {
   en: { label: "English" },
-  "en-GB": { label: "English (UK)" },
   de: { label: "Deutsch" },
   es: { label: "Español" },
-  "es-419": { label: "Español (Latinoamérica)" },
   fr: { label: "Français" },
-  "fr-FR": { label: "Français (France)" },
   id: { label: "Bahasa Indonesia" },
   it: { label: "Italiano" },
-  "it-IT": { label: "Italiano (Italia)" },
   ja: { label: "日本語" },
-  ko: { label: "한국어" },
-  nb: { label: "Norsk Bokmål" },
-  nl: { label: "Nederlands" },
   pl: { label: "Polski" },
   "pt-BR": { label: "Português (Brasil)" },
   "pt-PT": { label: "Português (Portugal)" },
   ru: { label: "Русский" },
-  sco: { label: "Scots" },
   "uk-UA": { label: "Українська" },
   "zh-CN": { label: "中文（简体）" },
 } as const;
 
-const localeLoaders: Record<keyof typeof webLocaleLookup, () => Promise<L10NLookup>> = {
+const localeLoaders: Record<
+  keyof typeof webLocaleLookup,
+  () => Promise<L10NLookup>
+> = {
   en: async () => en,
-  "en-GB": async () => (await import("lang/en-GB.json")).default as L10NLookup,
   de: async () => (await import("lang/de.json")).default as L10NLookup,
   es: async () => (await import("lang/es.json")).default as L10NLookup,
-  "es-419": async () => (await import("lang/es-419.json")).default as L10NLookup,
   fr: async () => (await import("lang/fr.json")).default as L10NLookup,
-  "fr-FR": async () => (await import("lang/fr-FR.json")).default as L10NLookup,
   id: async () => (await import("lang/id.json")).default as L10NLookup,
   it: async () => (await import("lang/it.json")).default as L10NLookup,
-  "it-IT": async () => (await import("lang/it-IT.json")).default as L10NLookup,
   ja: async () => (await import("lang/ja.json")).default as L10NLookup,
-  ko: async () => (await import("lang/ko.json")).default as L10NLookup,
-  nb: async () => (await import("lang/nb.json")).default as L10NLookup,
-  nl: async () => (await import("lang/nl.json")).default as L10NLookup,
   pl: async () => (await import("lang/pl.json")).default as L10NLookup,
   "pt-BR": async () => (await import("lang/pt-BR.json")).default as L10NLookup,
   "pt-PT": async () => (await import("lang/pt-PT.json")).default as L10NLookup,
   ru: async () => (await import("lang/ru.json")).default as L10NLookup,
-  sco: async () => (await import("lang/sco.json")).default as L10NLookup,
   "uk-UA": async () => (await import("lang/uk-UA.json")).default as L10NLookup,
   "zh-CN": async () => (await import("lang/zh-CN.json")).default as L10NLookup,
 };
@@ -109,7 +96,9 @@ export const getStoredThemeId = () => {
 
 export const getStoredLocaleId = () => {
   const locale = getStoredSetting(LOCALE_SETTING_KEY);
-  return typeof locale === "string" && locale in webLocaleLookup ? locale : "en";
+  return typeof locale === "string" && locale in webLocaleLookup
+    ? locale
+    : "en";
 };
 
 export const getThemeById = (themeId: string): ThemeInterface => {
