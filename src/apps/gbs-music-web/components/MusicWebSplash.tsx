40d0a5dc1107da6e9b3f7025dc2c56dc00881a39
Chris Maltby <chris.maltby@gmail.com>
2026-04-09 12:37:53 +0100

    WIP examples pane initial setup

--- src/apps/gbs-music-web/components/MusicWebSplash.tsx
+++ src/apps/gbs-music-web/components/MusicWebSplash.tsx
@@ -147,71 +147,72 @@ export const MusicWebSplash = ({
             {/* <SplashOpenButton onClick={() => {}}>Open File</SplashOpenButton> */}
             {/* <SplashOpenButton onClick={() => {}}>Open Folder</SplashOpenButton> */}
           </SplashSidebar>
-          <SplashContent>
-            {/* <SplashLoading>
-            <LoadingIcon />
-          </SplashLoading> */}
 
-            <div style={{ display: "flex", flexDirection: "column", gap: 20 }}>
-              <div>
-                <Select
-                  value={{ value: "workspace", label: "Workspace" }}
-                  options={[
-                    { value: "workspace", label: "Workspace" },
-                    { value: "single", label: "Single File" },
-                  ]}
-                />
-              </div>
+          {section === "new" && (
+            <SplashContent>
               <div
-                style={{ display: "flex", gap: 10, justifyContent: "center" }}
+                style={{ display: "flex", flexDirection: "column", gap: 20 }}
               >
-                {onCreateSong ? (
-                  <Button
-                    style={{ flexGrow: 1 }}
-                    size="large"
-                    variant="primary"
-                    onClick={onCreateSong}
-                  >
-                    {l10n("TOOL_ADD_SONG_LABEL")}
-                  </Button>
-                ) : null}
-                {onImportSong ? (
-                  <Button
-                    style={{ flexGrow: 1 }}
-                    size="large"
-                    variant="primary"
-                    onClick={onImportSong}
-                  >
-                    {l10n("FIELD_OPEN_FILE")}
-                  </Button>
-                ) : null}
-                {onOpenDirectoryWorkspace ? (
-                  <Button
-                    style={{ flexGrow: 1 }}
-                    size="large"
-                    variant="primary"
-                    onClick={onOpenDirectoryWorkspace}
-                  >
-                    {l10n("FIELD_OPEN_FOLDER")}
-                  </Button>
+                <div>
+                  <Select
+                    value={{ value: "workspace", label: "Workspace" }}
+                    options={[
+                      { value: "workspace", label: "Workspace" },
+                      { value: "single", label: "Single File" },
+                    ]}
+                  />
+                </div>
+                <div
+                  style={{ display: "flex", gap: 10, justifyContent: "center" }}
+                >
+                  {onCreateSong ? (
+                    <Button
+                      style={{ flexGrow: 1 }}
+                      size="large"
+                      variant="primary"
+                      onClick={onCreateSong}
+                    >
+                      {l10n("TOOL_ADD_SONG_LABEL")}
+                    </Button>
+                  ) : null}
+                  {onImportSong ? (
+                    <Button
+                      style={{ flexGrow: 1 }}
+                      size="large"
+                      variant="primary"
+                      onClick={onImportSong}
+                    >
+                      {l10n("FIELD_OPEN_FILE")}
+                    </Button>
+                  ) : null}
+                  {onOpenDirectoryWorkspace ? (
+                    <Button
+                      style={{ flexGrow: 1 }}
+                      size="large"
+                      variant="primary"
+                      onClick={onOpenDirectoryWorkspace}
+                    >
+                      {l10n("FIELD_OPEN_FOLDER")}
+                    </Button>
+                  ) : null}
+                </div>
+                {onRestoreBackup ? (
+                  <div style={{ display: "flex", justifyContent: "center" }}>
+                    <Button
+                      style={{ flexGrow: 1 }}
+                      size="large"
+                      variant="primary"
+                      onClick={onRestoreBackup}
+                    >
+                      {backupSongName
+                        ? `Restore backup: ${backupSongName}`
+                        : "Restore backup"}
+                    </Button>
+                  </div>
                 ) : null}
               </div>
-              {onRestoreBackup ? (
-                <div style={{ display: "flex", justifyContent: "center" }}>
-                  <Button
-                    style={{ flexGrow: 1 }}
-                    size="large"
-                    variant="primary"
-                    onClick={onRestoreBackup}
-                  >
-                    {backupSongName
-                      ? `Restore backup: ${backupSongName}`
-                      : "Restore backup"}
-                  </Button>
-                </div>
-              ) : null}
-            </div>
-          </SplashContent>
+            </SplashContent>
+          )}
         </SplashWindow>
 
         {openCredits && (
